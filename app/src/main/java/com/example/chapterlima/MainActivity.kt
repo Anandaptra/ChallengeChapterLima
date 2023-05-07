b068d48174ed74fb2c6ec18dafce76ff994ae05a
BAGAS <anndptra658@gmail.com>
2023-05-07 15:05:34 +0700

    Challenge Chapter Lima

---
+++ app/src/main/java/com/example/chapterlima/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.chapterlima
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import com.example.chapterlima.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        val crashButton = Button(this)
+//        crashButton.text = "Test Crash"
+//        crashButton.setOnClickListener {
+//            throw RuntimeException("Test Crash") // Force a crash
+//        }
+//
+//        addContentView(crashButton, ViewGroup.LayoutParams(
+//            ViewGroup.LayoutParams.MATCH_PARENT,
+//            ViewGroup.LayoutParams.WRAP_CONTENT))
+    }
+}
